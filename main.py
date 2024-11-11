08ef24ab06b62acd5cab1b49e5b0da4c5840a3d6
nivesh41 <nivesh1176@gmail.com>
2024-11-11 20:36:31 +0530

    updated code

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 print("Welcome to TicTacToe")
 print()
-grid = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
 
-for row in grid:
-    print(" | ".join(map(str, row)))
-    print("-" * 9)
+for i in range(3):
+    for j in range(3):
+        print("|   ", end="") 
+    print("|") 
+    if i < 2:
+        print("----" * 3) 
