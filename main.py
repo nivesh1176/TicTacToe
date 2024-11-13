e4fd14066e8425cd4b80fad95d76b0d4ca2309ca
nivesh41 <nivesh1176@gmail.com>
2024-11-13 22:27:17 +0530

    working with draw

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ def status():
     if input_grid[0:3]==["O","O","O"]or input_grid[3:6]==["O","O","O"] or input_grid[6:]==["O","O","O"]or input_grid[0:7:3]==["O","O","O"] or input_grid[1:8:3]==["O","O","O"] or input_grid[2:9:3]==["O","O","O"] or input_grid[0:9:4]==["O","O","O"]or input_grid[2:7:2]==["O","O","O"]:
         print("Player 2 wins")
     else: 
-        print("The match is a draw")
+        for i in range(9):
+            if i<9:
+                continue
+            else:
+                print("The match is a draw")
 
 print("Welcome to TicTacToe","\n")
 input_grid = [1,2,3,4,5,6,7,8,9]
