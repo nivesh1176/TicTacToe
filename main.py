2f1ae4ad7eaa0cb795049a850a3607f3fb5547ed
nivesh41 <nivesh1176@gmail.com>
2024-11-13 21:30:08 +0530

    updated status code

--- main.py
+++ main.py
@@ -12,23 +12,17 @@ draw_board(input_grid)
 p1 = int(input("P1 select value from 1 to 9: "))
 input_grid[p1-1]= "X"
 draw_board(input_grid)
-p1 = int(input("P1 select value from 1 to 9: "))
-input_grid[p1-1]= "X"
-draw_board(input_grid)
-p1 = int(input("P1 select value from 1 to 9: "))
-input_grid[p1-1]= "X"
-draw_board(input_grid)
 
 p2 = int(input("P2 select value from 1 to 9: "))
 input_grid[p2-1]= "O"
 draw_board(input_grid)
 
 def status():
-    if input_grid[0:3]==["X","X","X"]or input_grid[3:7]==["X","X","X"] or input_grid[6:]==["X","X","X"]or input_grid[0:7:3]==["X","X","X"] or input_grid[1:8:3]==["X","X","X"] or input_grid[2:9:3]==["X","X","X"] or input_grid[0:9:4]==["X","X","X"]or input_grid[2:7:2]==["X","X","X"]:
+    if input_grid[0:3]==["X","X","X"]or input_grid[3:6]==["X","X","X"] or input_grid[6:]==["X","X","X"]or input_grid[0:7:3]==["X","X","X"] or input_grid[1:8:3]==["X","X","X"] or input_grid[2:9:3]==["X","X","X"] or input_grid[0:9:4]==["X","X","X"]or input_grid[2:7:2]==["X","X","X"]:
         print("Player 1 wins")
-
+    if input_grid[0:3]==["O","O","O"]or input_grid[3:6]==["O","O","O"] or input_grid[6:]==["O","O","O"]or input_grid[0:7:3]==["O","O","O"] or input_grid[1:8:3]==["O","O","O"] or input_grid[2:9:3]==["O","O","O"] or input_grid[0:9:4]==["O","O","O"]or input_grid[2:7:2]==["O","O","O"]:
+        print("Player 2 wins")
     else: 
         print("The match is a draw")
 
 status()
-
