42bcfa22c8b62f7f57b6d5d99be9ccfa424b4c48
nivesh41 <nivesh1176@gmail.com>
2024-11-13 20:49:01 +0530

    working with status

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def board():
+def draw_board(grid):
     print("-------------")
     for i in range(len(grid)):
         print("|",grid[i],end=" ")
@@ -6,5 +6,29 @@ def board():
             print("|")
             print("-------------")
 print("Welcome to TicTacToe","\n")
-grid = [1,2,3,4,5,6,7,8,9]
-board()
+input_grid = [1,2,3,4,5,6,7,8,9]
+draw_board(input_grid)
+
+p1 = int(input("P1 select value from 1 to 9: "))
+input_grid[p1-1]= "X"
+draw_board(input_grid)
+p1 = int(input("P1 select value from 1 to 9: "))
+input_grid[p1-1]= "X"
+draw_board(input_grid)
+p1 = int(input("P1 select value from 1 to 9: "))
+input_grid[p1-1]= "X"
+draw_board(input_grid)
+
+p2 = int(input("P2 select value from 1 to 9: "))
+input_grid[p2-1]= "O"
+draw_board(input_grid)
+
+def status():
+    if input_grid[0:3]==["X","X","X"]or input_grid[3:7]==["X","X","X"] or input_grid[6:]==["X","X","X"]or input_grid[0:7:3]==["X","X","X"] or input_grid[1:8:3]==["X","X","X"] or input_grid[2:9:3]==["X","X","X"] or input_grid[0:9:4]==["X","X","X"]or input_grid[2:7:2]==["X","X","X"]:
+        print("Player 1 wins")
+
+    else: 
+        print("The match is a draw")
+
+status()
+
